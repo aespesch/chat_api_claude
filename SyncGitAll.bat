3a5d973da8b77f85dbdf90daa002e0a120a1971d
aespesch <antonio.espeschit@embraer.com.br>
2026-04-10 10:07:54 -0300

    Auto

---
+++ SyncGitAll.bat
@@ -0,0 +1,12 @@
+@echo off
+echo chat_api_claude
+cd /d D:\USER\Toni\Cursos\LLM\Claude\chat_api_claude
+call SyncGit.bat "Auto"
+
+echo Python
+cd /d d:\python
+call SyncGit.bat "Auto"
+
+echo Obsidian
+cd /d D:\USER\Toni\Obsidian
+call SyncGit.bat "Auto"
