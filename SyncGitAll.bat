5465d7f8a5c9f1e422b2c685a7d2b6c5167afb04
aespesch <antonio.espeschit@embraer.com.br>
2026-05-24 20:21:10 -0300

    Auto

--- SyncGitAll.bat
+++ SyncGitAll.bat
@@ -4,7 +4,7 @@ chcp 65001
 
 :: --- Configuração ---
 :: Lista de caminhos separados por ponto e vírgula
-set "folders=D:\USER\Toni\Financeiro\Site;D:\USER\Toni\Cursos\LLM\Claude\chat_api_claude;D:\python;D:\USER\Toni\Obsidian"
+set "folders=D:\USER\Toni\Financeiro\Site;D:\USER\Toni\Cursos\LLM\Claude\chat_api_claude;D:\python;D:\Obsidian"
 
 echo ============================================
 echo   Iniciando Sincronização Global do Git
